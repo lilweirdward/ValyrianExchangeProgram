207d8e8eb472a0993309a5230dcb2fa353908e28
Zach Woodward <zachw38@gmail.com>
2020-07-30 00:19:45 -0400

    Added a whole component for the Balance view

--- src/Braavos.Core/Entities/Balance.cs
+++ src/Braavos.Core/Entities/Balance.cs
@@ -9,14 +9,5 @@ namespace Braavos.Core.Entities
         public int Amount { get; set; }
         public BalanceType Type { get; set; }
         public Category Category { get; set; }
-
-        public override string ToString()
-        {
-            var balance = Amount.ToString();
-            balance = $"{(Category == Category.Credit ? "+" : "-")}{balance}";
-            balance = $"{balance}{(Type == BalanceType.Cash ? "$" : "")}";
-            balance = $"{balance} ({Category})";
-            return balance;
-        }
     }
 }
