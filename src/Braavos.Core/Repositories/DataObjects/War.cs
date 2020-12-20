77224c9514af96ecf7dafd6292aa4a5c627f0ffe
devoxwin <devoxwin@gmail.com>
2020-12-20 16:15:10 -0500

    War File Prep Work
    Fixed SQL Reference file to remove recent activity lines
    Added War function text file for future function.
    Added War.cs class

--- src/Braavos.Core/Repositories/DataObjects/War.cs
+++ src/Braavos.Core/Repositories/DataObjects/War.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Braavos.Core.Repositories.DataObjects
 {
-    public class Aid
+    public class War
     {
         public int DeclaringID { get; set; }
         public string DeclaringRuler { get; set; }
@@ -18,13 +18,14 @@ namespace Braavos.Core.Repositories.DataObjects
         public string ReceivingAlliance { get; set; }
         public int ReceivingAllianceID { get; set; }
         public string ReceivingTeam { get; set; }
-        public string Status { get; set; }
-        public decimal Money { get; set; }
-        public decimal Technology { get; set; }
-        public int Soldiers { get; set; }
-        public DateTime Date { get; set; }
+        public string WarStatus { get; set; }
+        public DateTime BeginDate { get; set; }
+        public DateTime EndDate { get; set; }
         public string Reason { get; set; }
-        public int AidID { get; set; }
+        public int WarID { get; set; }
+        public int Destruction { get; set; }
+        public int AttackPercent { get; set; }
+        public int DefendPercent { get; set; }
         public DateTime UpdatedOn { get; set; }
         public string UpdatedBy { get; set; }
 
