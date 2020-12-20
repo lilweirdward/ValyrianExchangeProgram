0feaf3f7b8b23da791ba8a8de33de405340192a9
devoxwin <devoxwin@gmail.com>
2020-12-20 14:08:35 -0500

    Aid File Import Resources
    -Added new file with function for aid import to \src\Braavos.Core\Repositories\DbContexts
    -Created Aid.cs in src\Braavos.Core\Repositories\DataObjects

---
+++ src/Braavos.Core/Repositories/DataObjects/Aid.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Braavos.Core.Repositories.DataObjects
+{
+    public class Aid
+    {
+        public int DeclaringID { get; set; }
+        public string DeclaringRuler { get; set; }
+        public string DeclaringNation { get; set; }
+        public string DeclaringAlliance { get; set; }
+        public int DeclaringAllianceID { get; set; }
+        public string DeclaringTeam { get; set; }
+        public int ReceivingID { get; set; }
+        public string ReceivingRuler { get; set; }
+        public string ReceivingNation { get; set; }
+        public string ReceivingAlliance { get; set; }
+        public int ReceivingAllianceID { get; set; }
+        public string ReceivingTeam { get; set; }
+        public string Status { get; set; }
+        public decimal Money { get; set; }
+        public decimal Technology { get; set; }
+        public int Soldiers { get; set; }
+        public DateTime Date { get; set; }
+        public string Reason { get; set; }
+        public int AidID { get; set; }
+        public RecentActivity RecentActivity { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+
+        //public object Alliance { get; set; }
+    }
+}
