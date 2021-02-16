9ed63854e61a9523cb24f51072eaeb7c8c5f0bb6
Zach Woodward <zachw38@gmail.com>
2021-02-16 00:23:36 -0500

    Implemented AcceptableRecipients property in the API

---
+++ src/Braavos.Core/Entities/ValidAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Braavos.Core.Entities
+{
+    public class ValidAccount : Account
+    {
+        public bool ShouldBeSendingAid { get; set; }
+
+        public ValidAccount() { }
+
+        public ValidAccount(Account account) : base(account) { }
+    }
+}
