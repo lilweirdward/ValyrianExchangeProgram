853ccf511f2cfc46be0232609a921c091cc4ddf3
Zach Woodward <zachw38@gmail.com>
2021-03-20 01:11:47 -0400

    Added an error screen to the account page since that's helpful

---
+++ src/Braavos.Ui/UiModels/PageException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Braavos.Ui
+{
+    public class PageException
+    {
+        public string Message { get; set; }
+        public string Exception { get; set; }
+
+        public PageException() { }
+
+        public PageException(string message, Exception exception)
+        {
+            Message = message;
+            Exception = exception.Message + Environment.NewLine + exception.StackTrace;
+        }
+    }
+}
